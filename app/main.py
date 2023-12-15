ce267f818fb6d12f95d77afa60441781e26cd91e
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2023-12-16 00:54:36 +0300

    готовый алгоритм молочницы

--- app/main.py
+++ app/main.py
@@ -4,48 +4,28 @@ from logic import get_all_amount, get_all_period
 
 from loguru import logger
 
-
+target = 100_000 # In USD
 base_take = 1000
 
 
 def main():
-    logger.debug(f"{base_take=}")
+    for i in range(target//base_take):
+        current_step = (get_all_period() + 1) * base_take
+        total_amount = get_all_amount()
+        usd_amount = total_amount * assert_data[i]
 
-    # Первый случай покупки
-    logger.debug(
-        f"Buy on {base_take} USD by {assert_data[0]} price is {base_take / assert_data[0]:.6f}"
-    )
-    insert_value_in_db(base_take / assert_data[0])
-    
-    for i in range(1, 100)[:15]:
-        logger.debug(f"Run is {i+1}")
-        full_steps = get_all_period() * base_take
-        total_amount = (
-            get_all_amount() * assert_data[i]
-        )  # Всего количество актива на сейчас в USD
-
-        logger.debug(f"{full_steps=} {get_all_amount()=:.6f} {total_amount=:.2f}")
-
-        if full_steps < total_amount:
-            # Количество ступени меньше чем у нас накопилось, нужно продать излишки
-            dif = base_take -  (total_amount - full_steps) # сколько нужно продать излишков
-            sell_size = dif / assert_data[i] # сколько это будет в активе
+        disition = current_step - usd_amount
+        
+        if disition > 0:
+            # Buy
+            logger.success(f"Buy on  \t${disition:.2f} \tby ${assert_data[i]:.3f}")
+        else:
+            # Sell
+            logger.error(f"Sell on \t${disition:.2f} \tby ${assert_data[i]:.3f}")
             
-            logger.debug(
-                f"Buy on {dif:.2f} USD by {assert_data[i]} price is {sell_size:.6f}"
-            )
-            insert_value_in_db(sell_size)
-
-        elif full_steps > total_amount:
-            # Количество ступени больше чем у нас накопилось, нужно докупить недостаток
-            dif = base_take + full_steps - total_amount # сколько нужно докупть до целей
-            buy_size = dif / assert_data[i] # сколько это будет в активе
-            logger.debug(
-                f"Buy on {dif:.2f} USD by {assert_data[i]} price is {buy_size:.6f}"
-            )
-            insert_value_in_db(buy_size)
+        insert_value_in_db(disition / assert_data[i])
 
         logger.debug("================================================================")
 
-
+    logger.debug(f"Total amount {get_all_amount()} coins")
 main()
