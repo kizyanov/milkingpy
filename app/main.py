42d114f2bf261092b077175ed0efea14aff21d12
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2023-12-17 01:47:56 +0300

    add decouple

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 from test import assert_data
 from db import count_records, sum_records, insert_value_in_db
 from loguru import logger
-
+from decouple import config
 import time
 
-target = 100_000  # In USD
-base_take = 1000
-
+TARGET = config("TARGET", cast=int)
+BASE_TAKE = config("BASE_TAKE", cast=int)
 
 def main():
-    for i in range(target // base_take):
-        current_step = (count_records() + 1) * base_take
+    for i in range(TARGET // BASE_TAKE):
+        current_step = (count_records() + 1) * BASE_TAKE
         total_amount = sum_records()
         logger.debug(total_amount)
         usd_amount = total_amount * assert_data[i]
@@ -19,10 +18,10 @@ def main():
 
         if disition > 0:
             # Buy
-            logger.success(f"Buy on  \t${disition:.2f} \tby ${assert_data[i]:.3f}")
+            logger.debug(f"Buy on  \t${disition:.2f} \tby ${assert_data[i]:.3f}")
         else:
             # Sell
-            logger.error(f"Sell on \t${disition:.2f} \tby ${assert_data[i]:.3f}")
+            logger.debug(f"Sell on \t${disition:.2f} \tby ${assert_data[i]:.3f}")
 
         insert_value_in_db(disition / assert_data[i])
 
