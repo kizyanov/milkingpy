d99748ba47adbd5f4471bf483bbfadaf70f15535
kizyanov <kizyanov@noreply.localhost>
2024-03-22 14:05:48 +0000

    Обновить app/main.py

--- app/main.py
+++ app/main.py
@@ -70,14 +70,14 @@ async def main():
                 if open_price > close_price:  # open price > close price
                     if db("count", candle.get("symbol")) == 1:
                         db("remove", candle.get("symbol"))
-                        msg = f'Sell \t\t{candle.get("symbol")} \t{open_price=} \t{close_price=} pr:{(open_price/close_price) * 100 -100:.3f}%'
+                        msg = f'Sell \t\t{candle.get("symbol")} \t\t{open_price=} \t\t{close_price=} \t\tpr:{(open_price/close_price) * 100 -100:.3f}%'
                         logger.debug(msg)
                         await send_telegram_msg(msg)
 
                 elif open_price < close_price:
                     if db("count", candle.get("symbol")) == 0:
                         db("update", candle.get("symbol"), funds=199.22)
-                        msg = f'Buy \t\t{candle.get("symbol")} \t{open_price=} \t{close_price=} pr:{(close_price/open_price) * 100 -100:.3f}%'
+                        msg = f'Buy \t\t{candle.get("symbol")} \t\t{open_price=} \t\t{close_price=} \t\tpr:{(close_price/open_price) * 100 -100:.3f}%'
                         logger.debug(msg)
                         await send_telegram_msg(msg)
 
