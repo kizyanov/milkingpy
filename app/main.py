bc99fdbc6d6da745518fa10b9c598d547b0135b0
kizyanov <kizyanov@noreply.localhost>
2024-03-25 06:20:07 +0000

    Обновить app/main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ from interesticker import INTEREST_TICKET
 from tinydb import Query
 from kucoin.client import Market
 
+from datetime import datetime, UTC
+
 symbol_status = {}
 
 TIME_SCALP = "_1hour"
@@ -105,20 +107,49 @@ async def main1():
     client = Market()
 
     while True:
-        profit_dirty = 0
 
-        for i in INTEREST_TICKET:
-            data = float(client.get_24h_stats(i)["changeRate"])
-            logger.debug(f"{i} \t {data}")
-            profit_dirty += data
+        now = datetime.now(tz=UTC).strftime("%H.%M.%S")
+        if now in [
+            "00.00.00",
+            "01:00:00",
+            "02:00:00",
+            "03:00:00",
+            "04:00:00",
+            "05:00:00",
+            "06:00:00",
+            "07:00:00",
+            "08:00:00",
+            "09:00:00",
+            "10:00:00",
+            "11:00:00",
+            "12:00:00",
+            "13:00:00",
+            "14:00:00",
+            "15:00:00",
+            "16:00:00",
+            "17:00:00",
+            "18:00:00",
+            "19:00:00",
+            "20:00:00",
+            "21:00:00",
+            "22:00:00",
+            "23:00:00",
+            "24:00:00",
+        ]:
+            profit_dirty = 0
 
-        profit = profit_dirty / total_coins * 100
+            for i in INTEREST_TICKET:
+                data = float(client.get_24h_stats(i)["changeRate"])
+                logger.debug(f"{i} \t {data}")
+                profit_dirty += data
 
-        logger.debug(f"PROFIT: {profit:.2f} %")
+            profit = profit_dirty / total_coins * 100
 
-        await send_telegram_msg(f"Profit is: {profit:.2f}%")
+            logger.debug(f"PROFIT: {profit:.2f} %")
 
-        await asyncio.sleep(60 * 60)
+            await send_telegram_msg(f"Profit is: {profit:.2f}%")
+
+        await asyncio.sleep(1)
 
 
 asyncio.run(main1())
