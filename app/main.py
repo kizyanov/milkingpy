b937a92b9b8b51d09c5e2646a43a150c8ca0f3cf
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2024-04-27 00:53:39 +0300

    next fix

--- app/main.py
+++ app/main.py
@@ -64,6 +64,7 @@ headers_base = {
 
 
 for symbol in market.get_symbol_list_v2():
+    logger.debug(symbol)
     if "." in symbol["baseIncrement"] and "." in symbol["priceIncrement"]:
         order_book[symbol["symbol"]] = {
             "baseIncrement": len(symbol["baseIncrement"].split(".")[1]),
