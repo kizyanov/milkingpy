067b3dbf721f047bc63e5504f838cf0663ee64ae
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2024-04-28 15:29:46 +0300

    some refactoring

--- app/main.py
+++ app/main.py
@@ -102,31 +102,10 @@ async def send_telegram_msg(msg: str):
             },
         ) as response,
     ):
-        f = response.status
+        pass
 
 
-d = {"sell": "loss", "buy": "entry"}
-
-
-def get_payload(side: str, symbol: str, price: int, priceIncrement: str):
-    place = priceIncrement.split("1")[0].count("0")
-    return json.dumps(
-        {
-            "clientOid": "".join([each for each in str(uuid1()).split("-")]),
-            "side": side,
-            "type": "limit",
-            "stop": d[side],
-            "stopPrice": str(price),
-            "tradyType": "TRADE",
-            "price": str(price),
-            "timeInForce": "FOK",
-            "symbol": symbol,
-            "size": f"{float(base_stake / price):.{place}f}",
-        },
-    )
-
-
-def get_payload2(
+def get_payload(
     side: str,
     symbol: str,
     price: int,
@@ -151,46 +130,6 @@ def encrypted_msg(msg: str) -> str:
     ).decode()
 
 
-async def make_stop_limit_order(
-    side: str,
-    price: int,
-    method: str = "POST",
-    method_uri: str = "/api/v1/stop-order",
-):
-    """Make stop limit order by price."""
-
-    now_time = int(time.time()) * 1000
-
-    data_json = get_payload(
-        side=side,
-        symbol=f"{currency}-{base_stable}",
-        price=price,
-        priceIncrement="0.00001",
-    )
-
-    uri_path = method_uri + data_json
-    str_to_sign = str(now_time) + method + uri_path
-
-    headers = {
-        "KC-API-SIGN": encrypted_msg(str_to_sign),
-        "KC-API-TIMESTAMP": str(now_time),
-        "KC-API-PASSPHRASE": encrypted_msg(passphrase),
-    }
-    headers.update(**headers_base)
-
-    async with (
-        aiohttp.ClientSession() as session,
-        session.post(
-            urljoin(base_uri, method_uri),
-            headers=headers,
-            data=data_json,
-        ) as response,
-    ):
-        result = await response.json()
-        if result["code"] == "200000":
-            return result["data"]["orderId"]
-
-
 async def make_limit_order(
     side: str,
     price: int,
@@ -203,7 +142,7 @@ async def make_limit_order(
 
     now_time = int(time.time()) * 1000
 
-    data_json = get_payload2(
+    data_json = get_payload(
         side=side,
         symbol=symbol,
         price=price,
