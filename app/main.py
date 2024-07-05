cfd73112838a3c0e1a55770175968f9426014453
Кизянов Антон Олегович <kizianov.ao@svrauto.ru>
2024-07-05 14:45:54 +0300

    try fix error

--- app/main.py
+++ app/main.py
@@ -190,7 +190,7 @@ async def make_limit_order(
             data=data_json,
         ) as response,
     ):
-        pass
+        await response.json()
 
 
 async def change_account_balance(data: dict):
